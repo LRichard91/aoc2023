34a4b560c328055a4e42b43051efc8f3e4288fd0
LRichard91 <140975381+LRichard91@users.noreply.github.com>
2023-12-01 20:33:57 +0100

    Day 1 - Part 2 completed

--- 1/day1.py
+++ 1/day1.py
@@ -12,10 +12,31 @@ def main():
 
     sum = 0
     for word in input:
+        word = word.lower()
         number = ""
-        for char in word:
-            if char.isdigit():
-                number += char
+        for char in range(len(word)):
+            if word[char].isdigit():
+                number += word[char]
+            elif word[char:char+3] == "one":
+                number += "1"
+            elif word[char:char+3] == "two":
+                number += "2"
+            elif word[char:char+5] == "three":
+                number += "3"
+            elif word[char:char+4] == "four":
+                number += "4"
+            elif word[char:char+4] == "five":
+                number += "5"
+            elif word[char:char+3] == "six":
+                number += "6"
+            elif word[char:char+5] == "seven":
+                number += "7"
+            elif word[char:char+5] == "eight":
+                number += "8"
+            elif word[char:char+4] == "nine":
+                number += "9"
+            elif word[char:char+4] == "zero":
+                number += "0"
         if len(number) == 2:
             number = int(number)
         elif len(number) == 1:
