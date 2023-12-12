f340dc40be9f119de05fd02f3b48598d7db3974d
LRichard91 <140975381+LRichard91@users.noreply.github.com>
2023-12-12 17:02:35 +0100

    Complete parsing input - Day 5
        Completed the input parsing part for Day 5.

--- 5/day5.py
+++ 5/day5.py
@@ -19,7 +19,120 @@ def parse_input(filename, debug=False):
     # Parsed input is a dictionary containing lists of tuples like this:
     # {'seeds': [...], 'sts': [(t, f, i), ...], stf:[(t, f, i), ...], ...}
     parsed_input = {}
-    
+    sts_idx, stf_idx, ftw_idx, wtl_idx, ltt_idx, tth_idx, htl_idx = 0, 0, 0, 0, 0, 0, 0
+    for index, item in enumerate(input):
+        if item.find('seeds:') != -1:
+            idx = item.find(':') + 2 
+            seed_str = item[idx:]
+            seeds = []
+            for n in seed_str.split(' '):
+                seeds.append(int(n))
+            parsed_input['seeds'] = seeds 
+        if item == 'seed-to-soil map:':
+            sts_idx = index
+        elif item == 'soil-to-fertilizer map:':
+            stf_idx = index
+        elif item == 'fertilizer-to-water map:':
+            ftw_idx = index
+        elif item == 'water-to-light map:':
+            wtl_idx = index
+        elif item == 'light-to-temperature map:':
+            ltt_idx = index
+        elif item == 'temperature-to-humidity map:':
+            tth_idx = index
+        elif item == 'humidity-to-location map:':
+            htl_idx = index
+        else:
+            continue
+        
+    parsed_input['seed-to-soil'] = []
+    for item in input[sts_idx+1:stf_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['seed-to-soil'].append((t, f, i))
+
+    parsed_input['soil-to-fertilizer'] = []
+    for item in input[stf_idx+1:ftw_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['soil-to-fertilizer'].append((t, f, i))
+
+    parsed_input['fertilizer-to-water'] = []
+    for item in input[ftw_idx+1:wtl_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['fertilizer-to-water'].append((t, f, i))
+
+    parsed_input['water-to-light'] = []
+    for item in input[wtl_idx+1:ltt_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['water-to-light'].append((t, f, i))
+
+    parsed_input['light-to-temp'] = []
+    for item in input[ltt_idx+1:tth_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['light-to-temp'].append((t, f, i))
+
+    parsed_input['temp-to-hum'] = []
+    for item in input[tth_idx+1:htl_idx]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['temp-to-hum'].append((t, f, i))
+
+    parsed_input['hum-to-loc'] = []
+    for item in input[htl_idx+1:]:
+        t, f, i = 0, 0, 0
+        for idx, n in enumerate(item.split(' ')):
+            if idx == 0:
+                t = int(n)
+            elif idx == 1:
+                f = int(n)
+            else:
+                i = int(n)
+        parsed_input['hum-to-loc'].append((t, f, i))
+
+    if debug:
+        print('The parsed input:')
+        pprint(parsed_input)
+        print('')
     return
 
 
